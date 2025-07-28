b3568e5d20fae2db37f6f1f602dee5db27c35557
Kojirouuuu <25nm106l@vc.ibaraki.ac.jp>
2025-07-28 16:20:57 +0900

    feat: network

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
+import main.java.network.Graph;
+import main.java.network.topology.RR;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    
 }
